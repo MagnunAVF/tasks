310d73e5ec31608031779cb720b783050b3e60e4
Magnun A V F <magnunavf@gmail.com>
2023-08-18 02:19:22 -0300

    feat (client): Create Footer and Header

---
+++ tasks-client/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="w-full p-4 bg-green-500 text-white mt-auto">
+      <h3 className="text-center">Magnun AVF ©</h3>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ tasks-client/src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="w-full p-4 bg-green-500 text-white">
+      <h1 className="text-lg">Tasks</h1>
+    </header>
+  )
+}
+
+export default Header
