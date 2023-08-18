d882c4c4b8dc6eb45cbea7e60a745d73f3a50649
Magnun A V F <magnunavf@gmail.com>
2023-08-18 02:19:48 -0300

    feat (client): Create responsive layout

--- tasks-client/src/pages/_document.tsx
+++ tasks-client/src/pages/_document.tsx
@@ -1,12 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
+      <body className="flex flex-col min-h-screen bg-white text-green-500">
+        <Header />
+
+        <main className="container mx-auto p-4">
+          <Main />
+
+          <NextScript />
+        </main>
+
+        <Footer />
       </body>
     </Html>
   )
