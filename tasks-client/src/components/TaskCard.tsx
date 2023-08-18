788beef3177b8991eba18314c2ab609d816a049b
Magnun A V F <magnunavf@gmail.com>
2023-08-18 03:21:14 -0300

    feat (client): Create home page
    - List all tasks.

---
+++ tasks-client/src/components/TaskCard.tsx
@@ -0,0 +1,22 @@
+import { Task } from "@/interfaces/task";
+
+const TaskCard = ({ task }: { task: Task}) => {
+    return (
+        <div className="bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-lg font-semibold mb-2">{task.title}</h3>
+            <p className="text-gray-600 mb-2">{task.description}</p>
+            <p className="text-gray-300 text-xs">Created at: <br/>{task.createdAt.toString()}</p>
+            <p className="text-gray-300 text-xs">Updated at: <br/>{task.updatedAt.toString()}</p>
+
+            {task.done ?
+                (<span className="bg-green-500 text-white px-2 py-1 mt-2 inline-block rounded">
+                    DONE
+                </span>) :
+                (<span className="bg-red-500 text-white px-2 py-1 mt-2 inline-block rounded">
+                    NOT DONE
+                </span>)}
+        </div>
+    );
+};
+
+export default TaskCard;
--- tasks-client/src/pages/index.tsx
+++ tasks-client/src/pages/index.tsx
@@ -1,13 +1,36 @@
+import { GetServerSideProps } from 'next';
 import { Inter } from 'next/font/google'
+
+import { Task } from '@/interfaces/task';
+import TaskCard from '@/components/TaskCard';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const runtime = 'experimental-edge';
 
-export default function Home() {
+export default function Home({ tasks }: { tasks: Task[]}) {
   return (
-    <main className="flex-grow p-4">
-      <h2>Main Content</h2>
+    <main className={`flex-grow p-4 ${inter.className}`}>
+        <section className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
+            <h3 className="text-2xl font-bold">Tasks List</h3>
+            <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded">Create</button>
+        </section>
+
+        <section className="mt-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {tasks.map((task, index) => (
+            <TaskCard key={index} task={task} />
+          ))}
+        </section>
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  tasks: Task[]
+}> = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BFF_API_URL}/tasks`);
+  const respBody = await res.json();
+  const tasks = respBody.data;
+
+  return { props: { tasks } };
 }
